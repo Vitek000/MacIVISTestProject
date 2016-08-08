90d47464d8304989dcc3b1c216c95267c72aff7e
valeksandrov <Qwer4321>
2016-08-08 19:02:50 +0300

    AvitoParser dev

--- src/main/java/velosearch/AvitoParserMain.java
+++ src/main/java/velosearch/AvitoParserMain.java
@@ -25,9 +25,8 @@ public class AvitoParserMain {
 //        simpleSearch2();
 //        simpleSearch3();
 
-        simpleSearch4();
-
-
+//        simpleSearch4();
+        simpleSearch5();
 
 
     }
@@ -60,7 +59,6 @@ public class AvitoParserMain {
     }
 
 
-
     public static void simpleSearch2() {
         URL url;
         InputStream is = null;
@@ -85,8 +83,6 @@ public class AvitoParserMain {
             }
         }
     }
-
-
 
 
     public static void simpleSearch3() {
@@ -121,6 +117,14 @@ public class AvitoParserMain {
         parsePage(IOUtils.toString(resourceAsStream, "utf-8"));
 
     }
+
+
+    public static void simpleSearch5() throws IOException {
+        InputStream resourceAsStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("avito_page_search_example_orig.html");
+        parsePage3(IOUtils.toString(resourceAsStream, "utf-8"));
+
+    }
+
 
     private static void parsePage(String page) {
 
@@ -179,6 +183,112 @@ public class AvitoParserMain {
 
     }
 
+
+    private static void parsePage3(String page) {
+
+        Document doc = Jsoup.parse(page);
+//        Elements links = doc.select("a[href]");
+//        Elements media = doc.select("[src]");
+//        Elements imports = doc.select("link[href]");
+
+        System.out.println("start parsePage!!!");
+        Elements imports = doc.getElementsByClass("item");
+
+//        System.out.println(imports.attr("id"));
+
+
+        for (Element import_ : imports) {
+            System.out.println("==========");
+            System.out.println("id=" + import_.attr("id"));
+            System.out.println("data-type=" + import_.attr("data-type"));
+
+            Elements elementsBPhoto = import_.getElementsByClass("b-photo");
+            for (Element elementBPhoto : elementsBPhoto) {
+                Elements elementsPhotoWrapper = elementBPhoto.getElementsByClass("photo-wrapper");
+                for (Element elementPhotoWrapper : elementsPhotoWrapper) {
+                    System.out.println("href=" + elementPhotoWrapper.attr("href"));
+                    System.out.println("title=" + elementPhotoWrapper.attr("title"));
+                }
+                Elements elementsPhotoImg = elementBPhoto.getElementsByTag("img");
+                for (Element elementPhotoImg : elementsPhotoImg) {
+                    System.out.println("src=" + elementPhotoImg.attr("src"));
+                    System.out.println("alt=" + elementPhotoImg.attr("alt"));
+                    break; // only first
+                }
+            }
+
+            Elements elementsDescription = import_.getElementsByClass("description");
+            for (Element elementDescription : elementsDescription) {
+                Elements elementsItemDescriptionTitleLink = elementDescription.getElementsByClass("item-description-title-link");
+                for (Element elementItemDescriptionTitleLink : elementsItemDescriptionTitleLink) {
+                    System.out.println("href=" + elementItemDescriptionTitleLink.attr("href"));
+                    System.out.println("title=" + elementItemDescriptionTitleLink.attr("title"));
+                }
+
+                Elements elementsAbout = elementDescription.getElementsByClass("about");
+                for (Element elementAbout : elementsAbout) {
+                    System.out.println("text=" + elementAbout.text());
+                }
+
+                Elements elementsData = elementDescription.getElementsByClass("data");
+                for (Element elementData : elementsData) {
+                    System.out.println("text=" + elementData.text());
+                    Elements p = elementData.select("p");
+
+                    int i = 0;
+                    for (Element element : p) {
+                        if(i==0) {
+                            String text = element.text();
+                            if(text.contains("|")) {
+                                String[] arr = text.split("\\|");
+                                System.out.println("category=" + arr[0]);
+                                System.out.println("isCompany=" + arr[1]);
+                            }
+                        }
+                        System.out.println("p=" + element.text());
+
+                        i++;
+                    }
+
+                    Elements elementsClearfix = elementData.getElementsByClass("clearfix");
+                    for (Element elementClearfix : elementsClearfix) {
+                        Elements elementsDateC2 = elementClearfix.getElementsByClass("date");
+                        for (Element elementDateC2 : elementsDateC2) {
+                            System.out.println("publicationDate=" + elementDateC2.text());
+                        }
+                    }
+
+
+
+
+
+                }
+            }
+
+
+
+
+        }
+
+//        StringBuilder stringBuilder = new StringBuilder();
+//        stringBuilder.append("<html>");
+//        for (Element import_ : imports) {
+//
+//            //System.out.println(import_);
+//            String newImport = import_.toString().replaceAll("src=\"//", "src=\"https://");
+//            newImport = newImport.replaceAll("data-srcpath=\"//", "data-srcpath=\"");
+//            stringBuilder.append(newImport);
+//            stringBuilder.append("\n");
+//
+//        }
+//
+//        stringBuilder.append("</html>");
+//
+//        System.out.println(stringBuilder);
+
+        //System.out.println(imports);
+
+    }
 
 
 
---
+++ src/main/java/velosearch/AvitoSearchHelper.java
@@ -0,0 +1,20 @@
+package velosearch;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by demo on 08/08/16.
+ */
+public class AvitoSearchHelper {
+
+
+    public static Date parseAvitoDate(String strDate) {
+
+
+        SimpleDateFormat simpleDateFormatForDate = new SimpleDateFormat("");
+        return new Date();
+
+
+    }
+}
